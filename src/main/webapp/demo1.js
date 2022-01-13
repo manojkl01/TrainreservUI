13734703aecb10254e0fb3216b9cd124c6831c1a
Manoj Dilip <manoj_dilip@thbs.india.com>
2022-01-13 16:02:31 +0530

    Initial commit

---
+++ src/main/webapp/demo1.js
@@ -0,0 +1,8 @@
+var movies = {
+        "name":"Pirates of Carrebian and the curse of Black perl",
+        "actor":"Johnny Depp",
+        "release_year":"2003"
+         };
+document.getElementById("result1").innerHTML = movies.name;
+document.getElementById("result2").innerHTML = movies.actor;
+document.getElementById("result3").innerHTML = movies.release_year;
